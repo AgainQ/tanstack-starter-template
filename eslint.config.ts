13de9e6d057073b1f693fb25f69741c20cad02d2
Kirill Drygin <godshades6@gmail.com>
2026-05-22 20:31:27 +0700

    chore: add query eslint plugin

--- eslint.config.ts
+++ eslint.config.ts
@@ -4,6 +4,7 @@ import { defineConfig, globalIgnores } from 'eslint/config';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import pluginRouter from '@tanstack/eslint-plugin-router';
+import pluginQuery from '@tanstack/eslint-plugin-query';
 import boundaries from 'eslint-plugin-boundaries';
 import { boundariesSettings, boundariesRules } from './eslint.boundaries';
 import { projectRules } from './eslint.rules';
@@ -21,6 +22,7 @@ export default defineConfig([
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
       pluginRouter.configs['flat/recommended'],
+      pluginQuery.configs['flat/recommended'],
       boundaries.configs.recommended,
     ],
     settings: {
